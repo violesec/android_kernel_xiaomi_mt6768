fdbdec5a840d6e95f4c52a47581d569fd12eb156
Viole403 <m.maulana.fb26@gmail.com>
2024-08-22 18:56:25 +0700

    Revert "refactor: KSU hooks"
    This reverts commit 601ccedea075eb56699dff27480b703017d685c4.
    
    	modified:   drivers/input/input.c
    	modified:   fs/devpts/inode.c
    	modified:   fs/exec.c
    	modified:   fs/namespace.c
    	modified:   fs/read_write.c
    	modified:   fs/stat.c

--- fs/exec.c
+++ fs/exec.c
@@ -1727,10 +1727,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 
 #ifdef CONFIG_KSU
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 #endif
 
 	if (IS_ERR(filename))
--- fs/read_write.c
+++ fs/read_write.c
@@ -431,7 +431,6 @@ EXPORT_SYMBOL(kernel_read);
 
 // KernelSU hook
 #ifdef CONFIG_KSU
-extern bool ksu_vfs_read_hook __read_mostly;
 extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
 			       size_t *count_ptr, loff_t **pos);
 #endif
@@ -441,8 +440,7 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 	ssize_t ret;
 
 #ifdef CONFIG_KSU
-	if (unlikely(ksu_vfs_read_hook))
-		ksu_handle_vfs_read(&file, &buf, &count, &pos);
+	ksu_handle_vfs_read(&file, &buf, &count, &pos);
 #endif
 
 	if (!(file->f_mode & FMODE_READ))
